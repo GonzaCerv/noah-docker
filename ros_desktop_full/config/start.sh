9f4285d3166638fb57873d3e00f6db22ff8867ce
Gonzalo Cervetti <g.cervetti@ekumenlabs.com>
2020-06-25 09:05:38 -0300

    mDNS setup for docker

--- ros_desktop_full/config/start.sh
+++ ros_desktop_full/config/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Fallback to *nginx* if nothing is specified
+if [ -z "${MDNS_HOSTNAME}" ]; then
+    export MDNS_HOSTNAME='nginx'
+fi
+
+# Setup the hostname on the avahi config
+# (We use the HOSTNAME environment variable for this)
+sed "s/\(host-name=\).*/\1${MDNS_HOSTNAME}/g" \
+    -i /etc/avahi/avahi-daemon.conf
+
 # Required services for mDNS to work on debian
 /etc/init.d/dbus start
 /etc/init.d/avahi-daemon start
+
+# After execute commands, left the console open
+/bin/bash
